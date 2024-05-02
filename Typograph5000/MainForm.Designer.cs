a71d7be8c04f9056dd0dc6cbcef70bddee1cc7f6
ilya <121713022+rokokol@users.noreply.github.com>
2024-05-02 21:57:38 +0300

    Application done

---
+++ Typograph5000/MainForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Typograph5000
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.checkButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.fixPunctuationsMarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixWhitespacesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixQuotesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixHyphentoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixDashtoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixKtoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixSatanSocialNetworkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fixSymbolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox.Location = new System.Drawing.Point(0, 27);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(800, 423);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "Hello World";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // menu
+            // 
+            this.menu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("menu.BackgroundImage")));
+            this.menu.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9F);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.checkButton, this.copyToolStripMenuItem, this.settingsToolStripMenuItem });
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 24);
+            this.menu.TabIndex = 1;
+            this.menu.Text = "menu";
+            // 
+            // checkButton
+            // 
+            this.checkButton.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(80, 20);
+            this.checkButton.Text = "Проверить";
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.ForeColor = System.Drawing.Color.LightGray;
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.copyToolStripMenuItem.Text = "Копировать";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.autoCheckToolStripMenuItem, this.toolStripSeparator1, this.fixPunctuationsMarksToolStripMenuItem, this.fixWhitespacesToolStripMenuItem, this.fixQuotesToolStripMenuItem, this.fixHyphentoolStripMenuItem, this.fixDashtoolStripMenuItem, this.fixKtoolStripMenuItem, this.fixSatanSocialNetworkToolStripMenuItem, this.fixSymbolsToolStripMenuItem });
+            this.settingsToolStripMenuItem.ForeColor = System.Drawing.Color.LightGray;
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.settingsToolStripMenuItem.Text = "Настройки";
+            // 
+            // autoCheckToolStripMenuItem
+            // 
+            this.autoCheckToolStripMenuItem.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9F);
+            this.autoCheckToolStripMenuItem.Name = "autoCheckToolStripMenuItem";
+            this.autoCheckToolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.autoCheckToolStripMenuItem.Text = "Автоматическая правка";
+            this.autoCheckToolStripMenuItem.Click += new System.EventHandler(this.autoCheckToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(614, 6);
+            // 
+            // fixPunctuationsMarksToolStripMenuItem
+            // 
+            this.fixPunctuationsMarksToolStripMenuItem.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9F);
+            this.fixPunctuationsMarksToolStripMenuItem.Name = "fixPunctuationsMarksToolStripMenuItem";
+            this.fixPunctuationsMarksToolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixPunctuationsMarksToolStripMenuItem.Text = "Слитные слева со словом знаки препинания";
+            this.fixPunctuationsMarksToolStripMenuItem.Click += new System.EventHandler(this.fixPunctuationsMarksToolStripMenuItem_Click);
+            // 
+            // fixWhitespacesToolStripMenuItem
+            // 
+            this.fixWhitespacesToolStripMenuItem.Name = "fixWhitespacesToolStripMenuItem";
+            this.fixWhitespacesToolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixWhitespacesToolStripMenuItem.Text = "Не более одного пробела подряд";
+            this.fixWhitespacesToolStripMenuItem.Click += new System.EventHandler(this.fixWhitespacesToolStripMenuItem_Click);
+            // 
+            // fixQuotesToolStripMenuItem
+            // 
+            this.fixQuotesToolStripMenuItem.Name = "fixQuotesToolStripMenuItem";
+            this.fixQuotesToolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixQuotesToolStripMenuItem.Text = "Елочки вместо «\"»";
+            this.fixQuotesToolStripMenuItem.Click += new System.EventHandler(this.fixQuotesToolStripMenuItem_Click);
+            // 
+            // fixHyphentoolStripMenuItem
+            // 
+            this.fixHyphentoolStripMenuItem.Name = "fixHyphentoolStripMenuItem";
+            this.fixHyphentoolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixHyphentoolStripMenuItem.Text = "Дефиз без окраймляющих пробелов";
+            this.fixHyphentoolStripMenuItem.Click += new System.EventHandler(this.fixHyphentoolStripMenuItem_Click);
+            // 
+            // fixDashtoolStripMenuItem
+            // 
+            this.fixDashtoolStripMenuItem.Name = "fixDashtoolStripMenuItem";
+            this.fixDashtoolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixDashtoolStripMenuItem.Text = "Между тире и предыдущим словом непереносимый пробел";
+            this.fixDashtoolStripMenuItem.Click += new System.EventHandler(this.fixDashtoolStripMenuItem_Click);
+            // 
+            // fixKtoolStripMenuItem
+            // 
+            this.fixKtoolStripMenuItem.Name = "fixKtoolStripMenuItem";
+            this.fixKtoolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixKtoolStripMenuItem.Text = "Не хорошо и не окей, а k";
+            this.fixKtoolStripMenuItem.Click += new System.EventHandler(this.fixKtoolStripMenuItem_Click);
+            // 
+            // fixSatanSocialNetworkToolStripMenuItem
+            // 
+            this.fixSatanSocialNetworkToolStripMenuItem.Name = "fixSatanSocialNetworkToolStripMenuItem";
+            this.fixSatanSocialNetworkToolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixSatanSocialNetworkToolStripMenuItem.Text = "Не *Instagram (Издатель Meta признан экстремистом на территории РФ), а сатанистск" + "ая соц-сеть";
+            this.fixSatanSocialNetworkToolStripMenuItem.Click += new System.EventHandler(this.fixSatanSocialNetworkToolStripMenuItem_Click);
+            // 
+            // fixSymbolsToolStripMenuItem
+            // 
+            this.fixSymbolsToolStripMenuItem.Name = "fixSymbolsToolStripMenuItem";
+            this.fixSymbolsToolStripMenuItem.Size = new System.Drawing.Size(617, 22);
+            this.fixSymbolsToolStripMenuItem.Text = "Использовать адекватное количество восклицательных и вопросительных знаков, троет" + "очий";
+            this.fixSymbolsToolStripMenuItem.Click += new System.EventHandler(this.fixSymbolsToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu;
+            this.Name = "MainForm";
+            this.Text = "Typograph5000";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ToolStripMenuItem fixSymbolsToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem fixSatanSocialNetworkToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem fixDashtoolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fixHyphentoolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fixKtoolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem checkButton;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem fixWhitespacesToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem fixQuotesToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem fixPunctuationsMarksToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+
+        private System.Windows.Forms.ToolStripMenuItem autoCheckToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+
+        private System.Windows.Forms.MenuStrip menu;
+
+        private System.Windows.Forms.RichTextBox textBox;
+
+        #endregion
+    }
+}
---
+++ Typograph5000/MainForm.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Typograph5000
+{
+    public partial class MainForm : Form
+    {
+        public bool[] Settings { get; private set; }
+        public MainForm()
+        {
+            InitializeComponent();
+            Settings = new[]
+            {
+                false,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true
+            };
+            UpdateSelectedTools();
+        }
+
+        public string FixPunctuationsMarks(string input)
+        {
+            var regex = new Regex(@"\s+(?=[,.!?:;...])");
+            return regex.Replace(input, string.Empty);
+        }
+        
+        public string FixWhitespaces(string input)
+        {
+            var regex = new Regex(@" +");
+            return regex.Replace(input, " ");
+        }
+
+        public string FixQuotes(string input)
+        {
+            var regex = new Regex("\"(?=[^\\n]*\")");
+            input = regex.Replace(input, "«");
+
+            regex = new Regex("(?<=«[^\\n]*)\"");
+            input = regex.Replace(input, "»");
+            
+            regex = new Regex("\"(?=[^\\n]*»)");
+            input = regex.Replace(input, "«");
+
+            return input;
+        }
+
+        public string FixHyphen(string input)
+        {
+            var regex = new Regex(@"\s*-\s*");
+            return regex.Replace(input, "-");
+        }
+        
+        public string FixDash(string input)
+        {
+            var regex = new Regex(@"\s?—\s?");
+            return regex.Replace(input, "\u00a0— ");
+        }
+        
+        public string FixK(string input)
+        {
+            var regex = new Regex(@"(okay)|(хорошо)|(окей)|((?<= )к(?= ))", RegexOptions.IgnoreCase);
+            return regex.Replace(input, "k");
+        }
+
+        public string FixSatanSocialNetwork(string input)
+        {
+            var regex = new Regex(@"(instagram)|(инстаграмм?\w?)|(инст\w)", RegexOptions.IgnoreCase);
+            return regex.Replace(input, "сатанисткая соц-сеть");
+        }
+        
+        public string FixSymbols(string input)
+        {
+            var regex = new Regex(@"!{2,}");
+            input = regex.Replace(input, "!!!");
+
+            regex = new Regex(@"\?{2,}");
+            input = regex.Replace(input, "???");
+            
+            regex = new Regex(@"\.{2,}");
+            return regex.Replace(input, "...");
+        }
+
+        public void Check()
+        {
+            if (Settings[1])
+            {
+                textBox.Text = FixPunctuationsMarks(textBox.Text);
+            }
+            
+            if (Settings[2])
+            {
+                textBox.Text = FixWhitespaces(textBox.Text);
+            }
+            
+            if (Settings[3])
+            {
+                textBox.Text = FixQuotes(textBox.Text);
+            }
+            
+            if (Settings[4])
+            {
+                textBox.Text = FixHyphen(textBox.Text);
+            }
+            
+            if (Settings[5])
+            {
+                textBox.Text = FixDash(textBox.Text);
+            }
+            
+            if (Settings[6])
+            {
+                textBox.Text = FixK(textBox.Text);
+            }
+            
+            if (Settings[7])
+            {
+                textBox.Text = FixSatanSocialNetwork(textBox.Text);
+            }
+            
+            if (Settings[8])
+            {
+                textBox.Text = FixSymbols(textBox.Text);
+            }
+        }
+        
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox.Text);
+        }
+
+        private void MakeUnderline(ToolStripMenuItem e, bool make)
+        {
+            e.Font = make ? new Font(e.Font, FontStyle.Underline) :
+                new Font(e.Font, FontStyle.Regular & FontStyle.Underline);
+        }
+
+        private void UpdateSelectedTools()
+        {
+            MakeUnderline(autoCheckToolStripMenuItem, Settings[0]);
+            MakeUnderline(fixPunctuationsMarksToolStripMenuItem, Settings[1]);
+            MakeUnderline(fixWhitespacesToolStripMenuItem, Settings[2]);
+            MakeUnderline(fixQuotesToolStripMenuItem, Settings[3]);
+            MakeUnderline(fixHyphentoolStripMenuItem, Settings[4]);
+            MakeUnderline(fixDashtoolStripMenuItem, Settings[5]);
+            MakeUnderline(fixKtoolStripMenuItem, Settings[6]);
+            MakeUnderline(fixSatanSocialNetworkToolStripMenuItem, Settings[7]);
+            MakeUnderline(fixSymbolsToolStripMenuItem, Settings[8]);
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (Settings[0])
+            {
+                Check();
+            }
+        }
+
+        private void SetSettings(int num)
+        {
+            Settings[num] = !Settings[num];
+            UpdateSelectedTools();
+        }
+        
+        private void autoCheckToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(0);
+        }
+
+        private void fixPunctuationsMarksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(1);
+        }
+
+        private void fixWhitespacesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(2);
+        }
+
+        private void fixQuotesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(3);
+        }
+
+        private void fixHyphentoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(4);
+        }
+
+        private void fixDashtoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(5);
+        }
+
+        private void fixKtoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(6);
+        }
+
+        private void fixSatanSocialNetworkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(7);
+        }
+
+        private void fixSymbolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetSettings(8);
+        }
+    }
+}
